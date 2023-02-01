cc01c872a7b20ebce994a87d36d61a8ac8dce879
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-01 20:15:18 +0530

    Added:Assignment Problem

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace Emp_Wage_Comp
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Wage Computation Program");
+
+            Random random= new Random();
+            int ran=random.Next(0,2);
+
+            if(ran == 0 )
+            {
+                Console.WriteLine("Employee is present" );
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
