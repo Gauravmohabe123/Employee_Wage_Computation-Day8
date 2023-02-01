98b9cf95589c6976fdbbaf57825011847136bc20
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-01 20:07:47 +0530

    Added:Assignment Problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_Comp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Employee Wage Computation Program");
+        }
+    }
+}
